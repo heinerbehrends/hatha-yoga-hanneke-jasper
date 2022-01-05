ed1db9c4b251af5a02573b9951a4e1e0b793fd80
heinerbehrends <heinerbehrends@hotmail.com>
2022-01-05 22:36:54 +0100

    added data temporary data file

---
+++ src/data.tsx
@@ -0,0 +1,49 @@
+import { InfoCardProps as InfoCardProps } from './components/InfoCard';
+
+export const infoCards: InfoCardProps[] = [
+  {
+    color: 'blue',
+    title: `Hatha Yoga is rustgevend`,
+    text: `Saswitha yoga is een vorm van hatha yoga die uitgaat van de eenheid 
+van lichaam en bewustzijn, waarbij de adem de verbinding vormt. De adem wordt gebruikt als 
+middel om in jezelf die verbinding te ervaren. De adem is de dragende kracht in elke beweging. 
+Door concentratie op de adem ontstaat een meditatieve aandacht die leidt tot verstilling.`,
+  },
+  {
+    color: 'red',
+    title: 'Hatha Yoga is vitaliserend',
+    text: `In de beoefening staat de adem centraal. Door tijdens de oefeningen juist te leren ademen, kan energie door het hele lichaam gaan stromen. 
+In de yogahoudingen kom je je eigen grenzen tegen. Met de adem kun je als het ware door deze grenzen heen ademen. 
+Vastgezette spanningen kun je hierdoor loslaten. Je masseert het lichaam als het ware van binnenuit met je adem. Dit geeft ruimte, ontspanning en vitaliteit.`,
+  },
+  {
+    color: 'green',
+    title: 'Hatha Yoga is helend',
+    text: `Saswitha yoga is een vorm van hatha yoga die uitgaat van de eenheid van lichaam en bewustzijn, waarbij de adem de verbinding vormt. 
+De adem wordt gebruikt als middel om in jezelf die verbinding te ervaren. De adem is de dragende kracht in elke beweging. Door concentratie op de adem ontstaat een meditatieve aandacht die leidt tot verstilling. 
+Dit samenvallen van bewustzijn-adem-lichaam kan zorgen voor een gevoel van harmonie.`,
+  },
+];
+
+export const lessenCards = [
+  {
+    title: 'Groepslessen',
+    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+    cta: 'Boek nu je gratis proefles',
+  },
+  {
+    title: 'Privélessen',
+    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+    cta: 'Boek nu je eerste les',
+  },
+  {
+    title: 'Lessen voor bedrijven',
+    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+    cta: 'Boek nu u gratis consult',
+  },
+  {
+    title: 'Lessen voor scholieren',
+    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+    cta: 'Boek nu u gratis adviesgresprek',
+  },
+];
