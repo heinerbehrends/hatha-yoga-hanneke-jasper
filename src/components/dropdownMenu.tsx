aca67d1b5a0e81d9f46f1142e3fffd55299e540b
heinerbehrends <heinerbehrends@hotmail.com>
2022-01-05 22:38:16 +0100

    fixed hide Trigger at l breakpoint

--- src/components/dropdownMenu.tsx
+++ src/components/dropdownMenu.tsx
@@ -2,19 +2,30 @@ import React from 'react';
 import * as DropdownPrimitives from '@radix-ui/react-dropdown-menu';
 import { ChevronDownIcon } from '@modulz/radix-icons';
 import { Link } from 'gatsby';
+import { styled } from '../../stitches.config';
 import slugify from 'slugify';
 import NavItem, { navBarItemStyles } from './navItem';
 
 type DropdownMenuItems = { items: string[] };
 
+const Trigger = styled(DropdownPrimitives.Trigger, {
+  variants: {
+    hide: {
+      true: {
+        display: 'none',
+      },
+    },
+  },
+});
+
 export default function DropdownMenu({ items }: DropdownMenuItems) {
   return (
     <DropdownPrimitives.Root>
-      <DropdownPrimitives.Trigger>
-        <NavItem hideAtLarge>
+      <Trigger hide={{ '@l': true }}>
+        <NavItem>
           Lessen <ChevronDownIcon />
         </NavItem>
-      </DropdownPrimitives.Trigger>
+      </Trigger>
       <DropdownPrimitives.Content>
         {items.map((item, index) => (
           <Link to={slugify(item)} key={index}>
