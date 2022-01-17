5aa605cfa37c9a05680268d0c5f93eeba75ad10f
heinerbehrends <heinerbehrends@hotmail.com>
2022-01-17 15:27:28 +0100

    Added mobile styles

--- src/components/testimonial.tsx
+++ src/components/testimonial.tsx
@@ -3,34 +3,57 @@ import { GatsbyImage, IGatsbyImageData } from 'gatsby-plugin-image';
 import { styled } from '../../stitches.config';
 
 const TestimonialContainer = styled('figure', {
-  fontSize: '$xl',
+  fontSize: '$m',
   fontStyle: 'italic',
   display: 'flex',
   backgroundColor: '$white',
   lineHeight: 1.1,
-  border: '1px solid $blue',
   padding: '$s',
   marginTop: '$s',
-  borderRadius: '$s',
+  borderTop: '1px solid $blue',
   overflow: 'hidden',
   '& figcaption': {
-    fontSize: '$body',
+    fontSize: '$s',
+    fontStyle: 'normal',
   },
-
   '&:before': {
-    fontWeight: 'light',
     content: 'open-quote',
-    paddingRight: '$xl',
-    fontSize: '240px',
-    lineHeight: '80px',
-    transform: 'translateY(50px)',
+    fontSize: '100px',
+    paddingRight: '$m',
+    lineHeight: '60px',
+    transform: 'translateY(30px)',
+  },
+  '@s': {
+    border: '1px solid $blue',
+    borderRadius: '$s',
+    fontSize: '$xl',
+    '&:before': {
+      fontWeight: 'light',
+      content: 'open-quote',
+      paddingRight: '$xl',
+      fontSize: '240px',
+      lineHeight: '80px',
+      transform: 'translateY(50px)',
+    },
+    '& figcaption': {
+      fontSize: '$body',
+    },
   },
 });
 
 const ImageContainer = styled('div', {
+  borderRadius: '50%',
+  marginLeft: '$s',
   overflow: 'hidden',
   flexShrink: 0,
-  flexBasis: '90px',
+  height: '60px',
+  flexBasis: '60px',
+  marginRight: '$s',
+  '@s': {
+    marginRight: 0,
+    height: '90px',
+    flexBasis: '90px',
+  },
 });
 
 type TestimonialProps = {
