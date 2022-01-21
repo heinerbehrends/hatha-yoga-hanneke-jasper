8f0f20581c4b0604c9170372ed5ef2f9f4c53459
heinerbehrends <heinerbehrends@hotmail.com>
2022-01-21 15:21:22 +0100

    First commit: api function to send contact form data

---
+++ src/api/send-email.ts
@@ -0,0 +1,31 @@
+import { GatsbyFunctionRequest, GatsbyFunctionResponse } from 'gatsby';
+
+// type guard function to check if error object has message property
+function isError(something: any): something is NodeJS.ErrnoException {
+  return something instanceof Error;
+}
+export default function handler(
+  req: GatsbyFunctionRequest,
+  res: GatsbyFunctionResponse
+) {
+  console.log(typeof req.body.message);
+  const sgMail = require('@sendgrid/mail');
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: 'heiner.s.behrends@gmail.com', // Change to your recipient
+    from: 'heinerbehrends@hotmail.com', // Change to your verified sender
+    subject: `A new message from ${req.body.name} arrived`,
+    html: `Emailadres: ${req.body.email} <br /> Message: ${req.body.message}`,
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log('Email sent');
+    })
+    .catch((error: any) => {
+      if (isError(error)) {
+        console.error(error);
+      }
+    });
+  res.status(200).json({ hello: `world` });
+}
