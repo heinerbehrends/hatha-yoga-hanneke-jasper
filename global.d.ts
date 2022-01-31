6051c2d2a67793b79f7011d3c8255913f09222d6
heinerbehrends <heinerbehrends@hotmail.com>
2022-01-31 15:21:52 +0100

    Added types for generative utils

--- global.d.ts
+++ global.d.ts
@@ -4,3 +4,7 @@ declare module '*.svg' {
   const src: React.FC<React.SVGProps<SVGSVGElement>>;
   export default src;
 }
+declare module '@georgedoescode/generative-utils' {
+  const random: (arg1: Array<Type> | number, arg2?: number) => Type | number;
+  const createVoronoiTessellation: (arg: any) => any;
+}
