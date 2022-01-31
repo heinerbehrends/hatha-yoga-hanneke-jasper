fb38fdb7443fb3e6930eb2697e802f534424b4db
heinerbehrends <heinerbehrends@hotmail.com>
2022-01-31 12:18:14 +0100

    new component SubHeading

--- src/components/indexStyles.ts
+++ src/components/indexStyles.ts
@@ -1,12 +1,27 @@
 import { styled } from '../../stitches.config';
 
 export const Heading = styled('h1', {
-  fontSize: 'clamp(30px, 5vw, 48px)',
-  lineHeight: '33px',
+  fontFamily: 'Ostrich Sans',
+  fontSize: 'clamp(30px, 25vw, 64px)',
+  lineHeight: '0.9',
   textAlign: 'center',
   marginTop: '$3xl-resp',
   paddingX: '$s',
 });
+
+export const SubHeading = styled('h2', {
+  color: '$blueDark',
+  fontFamily: 'asap',
+  fontSize: '32px',
+  textAlign: 'center',
+  lineHeight: '$heading',
+  paddingX: '$s',
+  marginTop: '$l',
+  '@l': {
+    marginTop: '$xl',
+  },
+});
+
 export const Introduction = styled('section', {
   backgroundColor: '$white',
   paddingX: '$s',
