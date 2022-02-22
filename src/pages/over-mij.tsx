692c2d0d15af198666fa230a38157bbffff896b9
heinerbehrends <heinerbehrends@hotmail.com>
2022-02-22 13:33:30 +0100

    removed image

--- src/pages/over-mij.tsx
+++ src/pages/over-mij.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { GatsbyImage, IGatsbyImageData } from 'gatsby-plugin-image';
+import { IGatsbyImageData } from 'gatsby-plugin-image';
 import Layout from '../components/layout';
 import { graphql } from 'gatsby';
-import { css, styled } from '../../stitches.config';
-import { Heading } from '../components/indexStyles';
+import { styled } from '../../stitches.config';
+import { Heading, TextBox } from '../components/indexStyles';
 
 export const query = graphql`
   query OverMij {
@@ -25,46 +25,8 @@ export const query = graphql`
   }
 `;
 
-const HTMLContainer = styled('article', {
-  maxWidth: '720px',
-  marginX: 'auto',
-  marginTop: '$xl',
-  backgroundColor: '$white',
-  paddingX: '$3xl',
-  paddingY: '$xxl',
-  borderRadius: '$s',
-  border: '1px solid $coral',
-  '& h2': {
-    fontSize: '$xl',
-    marginY: '$s',
-  },
-  '& ul': {
-    marginY: '$s',
-  },
-  '& li': {
-    listStyleType: 'circle',
-    listStylePosition: 'inside',
-  },
-});
-
 const OverMijContainer = styled('section', {
   display: 'flex',
-});
-
-const imageStyles = css({
-  width: '200px',
-  height: '260px',
-  transform: 'translate(30px, 30px)',
-});
-
-const ImageContainer = styled('div', {
-  borderRadius: '50%',
-  backgroundColor: '$green',
-  width: '260px',
-  height: '260px',
-  overflow: 'hidden',
-  transform: 'translate(-75px)',
-  border: '1px solid $coral',
 });
 
 type OverMijData = {
@@ -89,23 +51,15 @@ type OverMijData = {
 
 export default function OverMij({ data }: OverMijData) {
   const { title, content } = data.wpPage;
-  const image = data.wpPage.featuredImage.node;
   return (
     <Layout>
       <Heading>{title}</Heading>
       <OverMijContainer>
-        <HTMLContainer
+        <TextBox
           dangerouslySetInnerHTML={{
             __html: content,
           }}
         />
-        <ImageContainer>
-          <GatsbyImage
-            alt={image.altText}
-            image={image.localFile.childImageSharp.gatsbyImageData}
-            className={imageStyles()}
-          />
-        </ImageContainer>
       </OverMijContainer>
     </Layout>
   );
