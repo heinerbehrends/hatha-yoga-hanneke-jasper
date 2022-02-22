6b9261ebaf8746a2da79512e38e41f06463e2e1f
heinerbehrends <heinerbehrends@hotmail.com>
2022-02-22 13:23:55 +0100

    Added container

--- src/components/ContactCards.tsx
+++ src/components/ContactCards.tsx
@@ -5,7 +5,12 @@ import {
 } from '@modulz/radix-icons';
 import React from 'react';
 import Button from './button';
-import { ContactCardsContainer, Adress, ContactCard } from './contactStyles';
+import {
+  ContactCardsContainer,
+  Adress,
+  ContactCardWrapper,
+  ContactCardInner,
+} from './contactStyles';
 import { SmallerHeading } from './infoCardStyles';
 
 type ContactCardsProps = {
@@ -19,52 +24,58 @@ export default function ContactCards(props: ContactCardsProps) {
   return (
     <ContactCardsContainer>
       <Adress>
-        <ContactCard>
-          <SmallerHeading>
-            <PaperPlaneIcon /> WhatsApp
-          </SmallerHeading>
-          Ik ben op WhatsApp! Je kunt me een bericht sturen naar{' '}
-          {props.telefoonnummer} of maak gebruik van onderstaande button.
-          <Button
-            as="a"
-            href={`https://api.whatsapp.com/send?phone=+316${nummerNaarDeZes}`}
-            size="big"
-            color="green"
-          >
-            Stuur nu een appje
-          </Button>
-        </ContactCard>
-        <ContactCard>
-          <SmallerHeading>
-            <ChatBubbleIcon /> Telefoon
-          </SmallerHeading>
-          {props.telefonischBereikbaar} Je kunt me bellen onder{' '}
-          {props.telefoonnummer}.
-          <Button
-            as="a"
-            href={`tel:+316${nummerNaarDeZes}`}
-            size="big"
-            color="green"
-          >
-            Bel nu
-          </Button>
-        </ContactCard>
-        <ContactCard>
-          <SmallerHeading>
-            <EnvelopeClosedIcon />
-            Email{' '}
-          </SmallerHeading>
-          Gebruik het contactformulier of stuur een mailtje naar{' '}
-          {props.emailadres}.
-          <Button
-            as="a"
-            href={`mailto:${props.emailadres}`}
-            size="big"
-            color="green"
-          >
-            Mail nu
-          </Button>
-        </ContactCard>
+        <ContactCardWrapper>
+          <ContactCardInner>
+            <SmallerHeading>
+              <PaperPlaneIcon /> WhatsApp
+            </SmallerHeading>
+            Ik ben op WhatsApp! Je kunt me een bericht sturen naar{' '}
+            {props.telefoonnummer} of maak gebruik van onderstaande button.
+            <Button
+              as="a"
+              href={`https://api.whatsapp.com/send?phone=+316${nummerNaarDeZes}`}
+              size="big"
+              color="green"
+            >
+              Stuur nu een appje
+            </Button>
+          </ContactCardInner>
+        </ContactCardWrapper>
+        <ContactCardWrapper>
+          <ContactCardInner>
+            <SmallerHeading>
+              <ChatBubbleIcon /> Telefoon
+            </SmallerHeading>
+            {props.telefonischBereikbaar} Je kunt me bellen onder{' '}
+            {props.telefoonnummer}.
+            <Button
+              as="a"
+              href={`tel:+316${nummerNaarDeZes}`}
+              size="big"
+              color="green"
+            >
+              Bel nu
+            </Button>
+          </ContactCardInner>
+        </ContactCardWrapper>
+        <ContactCardWrapper>
+          <ContactCardInner>
+            <SmallerHeading>
+              <EnvelopeClosedIcon />
+              Email{' '}
+            </SmallerHeading>
+            Gebruik het contactformulier of stuur een mailtje naar{' '}
+            {props.emailadres}.
+            <Button
+              as="a"
+              href={`mailto:${props.emailadres}`}
+              size="big"
+              color="green"
+            >
+              Mail nu
+            </Button>
+          </ContactCardInner>
+        </ContactCardWrapper>
       </Adress>
     </ContactCardsContainer>
   );
