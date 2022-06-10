d77497aafd56693127f6146f3b648e14840cd019
heinerbehrends <heinerbehrends@hotmail.com>
2022-06-10 11:52:52 +0200

    Add lesson pages

---
+++ src/pages/groepslessen.tsx
@@ -0,0 +1,40 @@
+import { graphql } from 'gatsby';
+import React from 'react';
+import { makeContactData } from '../data/contactData';
+import { makeLessonPageData } from '../data/lessonData';
+import { groepslessenData } from '../data/paginas/groepslessenData';
+import { makeTestimonialData } from '../data/testimonialsData';
+import LessonLayout from '../layouts/lessonLayout';
+import { ImageNode } from '../utils';
+
+export default function Groepslessen({ data: pictures }: ImageQuery) {
+  const { heiner, lessenfoto } = pictures;
+  const wpLes = makeLessonPageData(groepslessenData, lessenfoto);
+  const wpAanbeveling = { aanbeveling: makeTestimonialData(heiner) };
+  const wpPage = {
+    contactgegevens: makeContactData(),
+  };
+  return <LessonLayout data={{ wpLes, wpAanbeveling, wpPage }}></LessonLayout>;
+}
+
+type ImageQuery = {
+  data: {
+    heiner: ImageNode;
+    lessenfoto: ImageNode;
+  };
+};
+
+export const query = graphql`
+  query groepslessenQuery {
+    heiner: file(relativePath: { eq: "Heiner.jpg" }) {
+      childImageSharp {
+        gatsbyImageData(width: 90)
+      }
+    }
+    lessenfoto: file(relativePath: { eq: "Lessenfoto.jpg" }) {
+      childImageSharp {
+        gatsbyImageData(width: 1360)
+      }
+    }
+  }
+`;
---
+++ src/pages/voor-bedrijven.tsx
@@ -0,0 +1,40 @@
+import { graphql } from 'gatsby';
+import React from 'react';
+import { makeContactData } from '../data/contactData';
+import { makeLessonPageData } from '../data/lessonData';
+import { voorBedrijvenData } from '../data/paginas/voorBedrijvenData';
+import { makeTestimonialData } from '../data/testimonialsData';
+import LessonLayout from '../layouts/lessonLayout';
+import { ImageNode } from '../utils';
+
+export default function LessenVoorBedrijven({ data: pictures }: ImageQuery) {
+  const { heiner, lessenfoto } = pictures;
+  const wpLes = makeLessonPageData(voorBedrijvenData, lessenfoto);
+  const wpAanbeveling = { aanbeveling: makeTestimonialData(heiner) };
+  const wpPage = {
+    contactgegevens: makeContactData(),
+  };
+  return <LessonLayout data={{ wpLes, wpAanbeveling, wpPage }}></LessonLayout>;
+}
+
+type ImageQuery = {
+  data: {
+    heiner: ImageNode;
+    lessenfoto: ImageNode;
+  };
+};
+
+export const query = graphql`
+  query individueleLessenQuery {
+    heiner: file(relativePath: { eq: "Heiner.jpg" }) {
+      childImageSharp {
+        gatsbyImageData(width: 90)
+      }
+    }
+    lessenfoto: file(relativePath: { eq: "Lessenfoto.jpg" }) {
+      childImageSharp {
+        gatsbyImageData(width: 1360)
+      }
+    }
+  }
+`;
