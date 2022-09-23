0e503462cf60e6de1a040ea81747e7ebb5e80569
Annahatha <112392311+Annahatha@users.noreply.github.com>
2022-09-23 13:42:31 +0200

    Update voorKinderenData.ts

--- src/data/paginas/voorKinderenData.ts
+++ src/data/paginas/voorKinderenData.ts
@@ -9,32 +9,39 @@ export const voorKinderenData: LessonNode = {
     menuTekst: 'Voor kinderen',
   },
   excerpt: /* html */ `
-    Yoga voor kinderen biedt ontspanning, rust, zelfvertrouwen, creativiteit, 
-    uit het hoofd kunnen komen en in het lijf.
-    In de groepsles yoga voor kinderen bewegen we met plezier en verbeelding. 
+    Yoga voor kinderen biedt ontspanning, rust, zelfvertrouwen en creativiteit.
+    Het geeft kinderen mogelijkheid om uit het hoofd kunnen komen en in het lijf.
+    <br>In de yogales voor kinderen bewegen we met plezier en verbeelding.<br>
     <br>Het kind leert zichzelf beter kennen door yoga. Door het bewegen en allerlei houdingen 
     aan te nemen ontdekken ze wie ze zijn en wat ze ervaren. <br>
   `,
   content: /* html */ `
     <h3>Ontspanning voor lichaam en geest </h3>
-    Yoga voor kinderen biedt ontspanning, rust, zelfvertrouwen, creativiteit, 
-    uit het hoofd kunnen komen en in het lijf.
-    In de groepsles yoga voor kinderen bewegen we met plezier en verbeelding. 
-    Het kind leert zichzelf beter kennen door yoga <br>. Door het bewegen en allerlei houdingen 
-    aan te nemen ontdekken ze wie ze zijn en wat ze ervaren. <br>
+    <br>Yoga voor kinderen biedt ontspanning, rust, zelfvertrouwen en creativiteit. 
+    Kinderyoga geeft de mogelijkheid om uit het hoofd kunnen komen en in het lijf.<br)
     
+    <br>Het kind kan zichzelf beter leren kennen door yoga. Door het bewegen en allerlei houdingen 
+    aan te nemen ontdekken ze wie ze zijn en wat ze ervaren. <br>
+    <br>In de yogales voor kinderen bewegen we met plezier en verbeelding.<br>
+
+   
     <br>Er is bestaat een yogahouding voor van alles wat een kind ervaren kan: 
     Blijheid kan worden gevierd, boosheid/frustratie kan worden ontladen, moed 
     en kracht wordt gezocht in houdingen zoals de Held. 
     <br> 
-    In de yogalessen wordt fantasie en creativiteit aangesproken. Zelfvertrouwen 
+    <br>In de yogalessen wordt fantasie en creativiteit aangesproken. Zelfvertrouwen 
     wordt gestimuleerd doordat het kind zichzelf kan zijn en mag voelen wat er
     is. Het gaat nooit over 'moeten' in de lessen (behalve de afspraken om een 
     geconcentreerde en veilige sfeer te creëren). <br>
-    In de les kunnen de kinderen naast oefeningen die ik aanbied, zelf een 
-    kaartje met een oefening uitkiezen. Zo is er ruimte voor individuele 
+    <br>In de les kunnen de kinderen naast oefeningen die ik aanbied, zelf een 
+    kaartje met een oefening uitkiezen. Zo is er ook ruimte voor de individuele 
     behoefte. Het mooie hiervan is dat we ook nog oefenen om aandacht voor elkaar 
-    te hebben en elkaar ruimte te geven.
+    te hebben en elkaar ruimte te geven.<br>
+    
+  
+    
+    <br> Individuele lessen met uw kind (of samen met verzorger/ouder) zijn ook mogelijk.
+    Bijvoorbeeld als uw kind hoogsensitief is en/of lastig prikkels verwerkt.<br>
     `,
 };
 
