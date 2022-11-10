6caf94aad1411cfd5638fbecc7e21d3d8ac4db9b
heinerbehrends <heinerbehrends@hotmail.com>
2022-11-10 21:37:21 +0100

    Create ContactCardContainer

--- src/components/ContactCards.tsx
+++ src/components/ContactCards.tsx
@@ -20,68 +20,70 @@ type ContactCardsProps = {
   emailadres: string;
 };
 
+function ContactCardContainer({ children }: { children: React.ReactNode }) {
+  return (
+    <ContactCardWrapper>
+      <ContactCardInner>{children}</ContactCardInner>
+    </ContactCardWrapper>
+  );
+}
+
 export default function ContactCards(props: ContactCardsProps) {
   const nummerNaarDeZes = props.telefoonnummer.split('-')[1];
   return (
     <ContactCardsContainer>
       <Adress>
-        <ContactCardWrapper>
-          <ContactCardInner>
-            <SmallerHeading>
-              <PaperPlaneIcon /> WhatsApp
-            </SmallerHeading>
-            Ik ben op WhatsApp! Je kunt me een bericht sturen naar{' '}
-            {props.telefoonnummer}{' '}
-            <HideAboveMobile>
-              of maak gebruik van onderstaande button.
-            </HideAboveMobile>
-            <Button
-              as="a"
-              href={`https://api.whatsapp.com/send?phone=+316${nummerNaarDeZes}`}
-              size="big"
-              hide="aboveMobile"
-              color="green"
-            >
-              Stuur nu een appje
-            </Button>
-          </ContactCardInner>
-        </ContactCardWrapper>
-        <ContactCardWrapper>
-          <ContactCardInner>
-            <SmallerHeading>
-              <ChatBubbleIcon /> Telefoon
-            </SmallerHeading>
-            {props.telefonischBereikbaar} Je kunt me bellen onder{' '}
-            {props.telefoonnummer}.
-            <Button
-              as="a"
-              href={`tel:+316${nummerNaarDeZes}`}
-              size="big"
-              color="green"
-              hide="aboveMobile"
-            >
-              Bel nu
-            </Button>
-          </ContactCardInner>
-        </ContactCardWrapper>
-        <ContactCardWrapper>
-          <ContactCardInner>
-            <SmallerHeading>
-              <EnvelopeClosedIcon />
-              Email{' '}
-            </SmallerHeading>
-            Gebruik het contactformulier of stuur een mailtje naar{' '}
-            {props.emailadres}.
-            <Button
-              as="a"
-              href={`mailto:${props.emailadres}`}
-              size="big"
-              color="green"
-            >
-              Mail nu
-            </Button>
-          </ContactCardInner>
-        </ContactCardWrapper>
+        <ContactCardContainer>
+          <SmallerHeading>
+            <PaperPlaneIcon /> WhatsApp
+          </SmallerHeading>
+          Ik ben op WhatsApp! Je kunt me een bericht sturen naar{' '}
+          {props.telefoonnummer}{' '}
+          <HideAboveMobile>
+            of maak gebruik van onderstaande button.
+          </HideAboveMobile>
+          <Button
+            as="a"
+            href={`https://api.whatsapp.com/send?phone=+316${nummerNaarDeZes}`}
+            size="big"
+            hide="aboveMobile"
+            color="green"
+          >
+            Stuur nu een appje
+          </Button>
+        </ContactCardContainer>
+        <ContactCardContainer>
+          <SmallerHeading>
+            <ChatBubbleIcon /> Telefoon
+          </SmallerHeading>
+          {props.telefonischBereikbaar} Je kunt me bellen onder{' '}
+          {props.telefoonnummer}.
+          <Button
+            as="a"
+            href={`tel:+316${nummerNaarDeZes}`}
+            size="big"
+            color="green"
+            hide="aboveMobile"
+          >
+            Bel nu
+          </Button>
+        </ContactCardContainer>
+        <ContactCardContainer>
+          <SmallerHeading>
+            <EnvelopeClosedIcon />
+            Email{' '}
+          </SmallerHeading>
+          Gebruik het contactformulier of stuur een mailtje naar{' '}
+          {props.emailadres}.
+          <Button
+            as="a"
+            href={`mailto:${props.emailadres}`}
+            size="big"
+            color="green"
+          >
+            Mail nu
+          </Button>
+        </ContactCardContainer>
       </Adress>
     </ContactCardsContainer>
   );
